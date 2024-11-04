a41d00ad618c7374ccb74ef8af20f4b07aa0ba22
Nick30035 <nicholas@bbcdayschool.org.in>
2024-11-04 18:57:10 +0000

    asyncActivityJava

---
+++ classNotes/asyncActivity_nov1.js
@@ -0,0 +1,11 @@
+// Strings
+// Var variables can be updated and re-declared, let variables can be updated but not re-declared and const variables can't be updated nor re-declared.
+
+console.log(200*10*2)
+
+// We use the let variable in a scenario that we know the value of the variable will change.
+// We use const variable in a scenario that we know the value of the variable can't be changed.
+
+// More of a serious working enviornment could help
+// Maybe some more one-on-one attention could also help us in our learning experience
+// More practice when it comes to coding
---
+++ classNotes/jsPractice.js
@@ -0,0 +1,22 @@
+// Data types are basic building
+// block of Java Script programs
+
+
+// Strings
+// strings are a piece of data that is wrapped in 
+// quotation marks.
+
+"double quotes tell the computer that this is a string"
+"My name is Nick"
+'single quotes can also be used to write quotes'
+console.log('my name is Nick');
+
+// Integers
+// Integers are pieces of data that represent numbers.
+30
+5
+10
+1093333333992
+console.log(5+10)
+
+// Booleans
