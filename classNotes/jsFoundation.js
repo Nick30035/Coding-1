058f7af5985497baa6699bbb61169208aea14aee
Nick30035 <nicholas@bbcdayschool.org.in>
2024-11-07 20:11:19 +0000

    JavaScriptVariableFormatting

---
+++ classNotes/jsFoundation.js
@@ -0,0 +1,31 @@
+var example1= 'this is a string 23082581'
+var example2= true//boolean = true and false
+var example3= 12345 // intergers= numbers
+
+// var is short for variable
+// it is theuniversal keyword for a variable
+// when you create a variable, you always start with 'var'
+// OR use let or const BUT these have special contexual usage
+
+// var is a keyword - meaning it is a word built into the JavaScript language
+// cannot use keywords in anyway other than 
+
+
+var example= 'Nick'
+var userAddress= '123 main street'
+var userPhoneNumberPrimary= 1297633
+
+
+// a variable name should be short, simple, yet descriptive
+// a variable name must start with a letter or underscore
+// CamelCase- every word is capitalized except the first word
+tiktokAccountName= 'nick034'
+// Pascal Case- every word is capitalized
+TikTokAccountName= 'dsfd'
+// Snake Case- every word is separated by an underscore
+tiktok_account_name= 'nick321'
+
+
+let='' // special usage- data is expected to change
+
+const='' // special usage - data DOES NOT change
