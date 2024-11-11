583d70cb80c8ce5fd4b647dad09ba3d2ae1d01d6
Nick30035 <nicholas@bbcdayschool.org.in>
2024-11-11 12:13:59 +0000

    asyncActivityAssignment11/8/24

---
+++ Unit 3/asyncActivityNov8.js
@@ -0,0 +1,19 @@
+// 1. What it means for something to be "sting concatenate" is when two different strings are combined.
+console.log ('John'); 
+console.log ('Doe');
+
+console.log ('Nicholas');
+console.log ('Saunders');
+2. // When you create a variable, you always start with 'var'
+// a variable name should be short, simple, yet descriptive
+// a variable name must start with a letter or underscore
+3. 
+githubAccountName = 'nick30035'
+github_account_name= 'nick30035'
+GitHubAccountName= 'nick30035'
+4. 
+console.log (350*2)
+5. 
+console.log (12/2)
+// I would use the divison operator to reflect that the result is false because it would give us our
+// answer to prove so.
