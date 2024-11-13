ea269170b0dc50b93ff182ddab928d2d194c033f
Nick30035 <nicholas@bbcdayschool.org.in>
2024-11-13 20:14:36 +0000

    ReviewingOperator

---
+++ Unit 3/operatorReview.js
@@ -0,0 +1,33 @@
+// Arithmetic operators - Writing code expressions that perform math operations
+
+// examples of apps that use arithmetic operators
+var depositAmount ='2000'
+var checkAccount ='3000'
+var savingAccount ='4000'
+
+console.log (depositAmount + checkAccount);
+// function - set of instructions that perform a job
+
+
+// peer to peer money transfers
+var amountBeingSent='';
+var userAccount= "";
+// Assignment - giving variables values.
+// we use the single equal sign (=) to assign data to variables
+
+// sneakers is the variable. We are ASSIGNING the value of 200.00 with the equal sign
+var sneakers= 200.00
+
+sneakers *= .30
+
+console.log (sneakers)
+
+// Comparison - analyzing values. checking to see if a certain condition is met.
+
+// =
+// ===
+// !=
+
+console.log(2+2)
+
+// Logical
