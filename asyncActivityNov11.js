d028fc49d8bd5fa8ef106f27a7ee43da8bc40371
Nick30035 <nicholas@bbcdayschool.org.in>
2024-11-15 20:11:42 +0000

    AsyncWorkUnfinished

---
+++ asyncActivityNov11.js
@@ -0,0 +1,12 @@
+// 1. The operator Logical
+// 2. The Assignment operator
+// console.log (ECG machines)
+// 3.
+// 4. The (=) operator in Javascript checks whether it's two operators are equal.
+// The (==) operator in Javascript compares the values of two variables after comparing type conversion
+// if necessary
+// The (||) operator in Javascript also known as the (logical disjunction) checks if one or more 
+// operators are true
+// The (&&) operator in Javascript is known as the logical AND operator, making sure both values are
+// on the left and right
+
