38e94e6794a3e7dbce8225bbbf3eac1b6469a485
Nick30035 <nicholas@bbcdayschool.org.in>
2024-11-19 20:13:18 +0000

    IntroToFunctionsJavaScript

---
+++ Unit 3/functionPractice/functions.js
@@ -0,0 +1,30 @@
+// What is a function?
+'A function is a set of instructions that perform a speciifc task or job.'
+
+// Functions come in 2 variations:
+// built-in functions - these are instructions that are pre-written into the coding language
+// all we need to do is pass data
+// examples
+var className= 'Coding class'
+console.log(className)
+
+// user-defined functions- custom functions written by engineers to make code that performs
+// specific tasks
+
+// How do we write a user-defined function?
+
+// function exampleFunction(){
+    console.log('the function script is connected')
+}
+
+
+// in order to have a function work, we need to call it the above,
+// code is the function definition- MEANING it's only telling the computer the instructions
+// not to actually run it
+
+// we access or call a function by writing its name
+exampleFunction()
+
+// var
+// let
+// const 
