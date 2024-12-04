33713965c205b4ca57a6decb33c0b1695f43106c
Nick30035 <nicholas@bbcdayschool.org.in>
2024-12-04 20:14:38 +0000

    timeManagementConsoleLogPractice

--- Unit 3/handsOnFunctions.js
+++ Unit 3/handsOnFunctions.js
@@ -36,5 +36,30 @@ pizzaParty('Nick','pepperoni')
 pizzaPartyInvitation('Nick')
 
 
+function hourToMinutes(hour,minutes){
+    console.log(hour* 60 +'minutes')
+}
 
+hourToMinutes(2)
+
+
+// Conditional Statements
+// If/Else keywords used create specific 
+// outcomes based on the data the function receives.
+
+function timeManagementSystem(hoursWorked){
+if(hoursWorked <8){
+    console.log("Unfortunately you will not get full time pay")
+} else if (hoursWorked >8){
+    console.log("You will be paid overtime");
+} else if (hoursWorked >16 ) {
+    console.log("You are working way too hard");
+} else 
+    console.log("You will get paid full time for the day.");{
+
+    }
+
+}
+
+timeManagementSystem(10)
 
