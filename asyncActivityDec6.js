1942a296b8d6d831b43b1bffbfb694af9e611f81
Nick30035 <nicholas@bbcdayschool.org.in>
2024-12-09 18:59:31 +0000

    asyncActivityDec6.js

---
+++ asyncActivityDec6.js
@@ -0,0 +1,28 @@
+<!DOCTYPE>
+<html>
+<body>
+
+
+<p id="demo"></p>
+
+<script> 
+    let= newWord = 'Nick'
+
+    function reverseWord(Nick){
+
+    }
+reverseWord('Kcin')
+
+
+
+document.getElementById("demo").innerHTML = myArray; 
+</script> 
+
+</body>
+</html>
+
+
+function stateLandMark({
+    let guest= 'Pennsylvania'
+    if()
+})
