1827a3a52f83d44c1b92c5890efcf2e83c9e1081
Nick30035 <nicholas@bbcdayschool.org.in>
2024-12-13 20:12:09 +0000

    asyncActivityDec13.js

---
+++ asyncActivityDec13.js
@@ -0,0 +1,11 @@
+function numbers(positive,negative){
+    console.log('congrats the number you chose is positive'+positive+'your number will be accepted');
+        console.log('Oh no!'+negative+'the number you chose is negative and wont be accepted');
+}
+
+numbers('7','-12')
+
+
+function movieProgram(agePrice){
+    console.log()
+}
