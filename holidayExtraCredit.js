b63cb44155a155da2fa2d604ee2dad6cff67c46a
Nick30035 <nicholas@bbcdayschool.org.in>
2024-12-19 20:20:32 +0000

    holidayExtraCredit.js

---
+++ holidayExtraCredit.js
@@ -0,0 +1,55 @@
+function holidaySchedulePlanner(date){
+    let guest= 'date';
+    if (date!=guest){
+        console.log("I'm going Christmas shopping with my family");
+    } else {
+        console.log("I have plans on this day")
+    }
+}
+holidaySchedulePlanner('12/21/24')
+
+
+function holidaySchedulePlanner(date){
+    let guest='date';
+    if (date!=guest){
+        console.log("I'm celebrating Christmas Eve with my family")
+    } else {
+        console.log("I have plans on this day")
+    }
+}
+holidaySchedulePlanner('12/24/24')
+
+
+function holidaySchedulePlanner(date){
+    let guest='date';
+    if (date!=guest){
+        console.log("I'm celebrating Christmas with my family")
+    } else {
+        console.log("I have plans on this day")
+    }
+}
+holidaySchedulePlanner('12/25/24')
+
+
+
+function holidaySchedulePlanner(date){
+    let guest='date';
+    if (date!=guest){
+        console.log("I'm chilling on this day")
+    } else{
+        console.log("I have no plans ")
+    }
+}
+holidaySchedulePlanner('12/31/24')
+
+
+
+function holidaySchedulePlanner(date){
+    let guest= 'date';
+    if (date!=guest){
+        console.log("I'm chilling on this day")
+    } else{
+        console.log("I have no plans ")
+    }
+}
+holidaySchedulePlanner('1/1/25')
