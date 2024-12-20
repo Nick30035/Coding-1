5ecf7afbd4dadf855a384c4fbabcd906f54ed1ce
Nick30035 <nicholas@bbcdayschool.org.in>
2024-12-20 20:19:08 +0000

    Quarter2.Quiz.js

---
+++ Quarter2.Quiz.js
@@ -0,0 +1,11 @@
+1. // A function is a set of instructions that perform a specific  task or job.
+2. // The difference between a function parameter and function agrument is that the parameter
+// are the names listed in the definition, whereas the function agrument are the real values
+// that are passed and carry out a function
+3. // The 'If' conditional statement allows us to check and see if a function is true
+// The 'Else' conditional statement allows us to check and see if a function is false
+4. // The integer data type is are pieces of data that represent whole numbers.
+5. // A Boolean data type is data that can store true or false values
+6. // The Comparison function could be used to verify the age
+7. // The Boolean operator could be used here to check if the code entered is true or false
+8. // 
--- asyncActivityNov11.js
+++ asyncActivityNov11.js
@@ -1,5 +1,6 @@
 // 1. The operator "Logical"
-// 2. The "Comparison" operator 'because we need to check and compare if we have more or less based of what we ordered'
+// 2. The "Comparison" operator 'because we need to check and compare if we have more or less based
+// of what we ordered'
 // 3. The "Assignment" operator 'because we use this to make messages
 // 4. =
 'single equal sign = Assignment operator'
