8e630f0f81e133564b0d7430c9426e5fa0baeb7b
Nick30035 <nicholas@bbcdayschool.org.in>
2025-01-07 20:24:14 +0000

    postHolidayReview.js

---
+++ Unit 4/postHolidayReview.js
@@ -0,0 +1,13 @@
+// data types - level 1: most basic building block of
+// code. (numbers, letter, true or false)
+
+
+// operators  -  level 2: the ability to manipulate and 
+// do things with data types
+// (math, comparisons, assignment, etc.)
+
+
+
+// functions -  level 3: taking the first two concepts and
+// organizing these operations and data types 
+// into instructions
