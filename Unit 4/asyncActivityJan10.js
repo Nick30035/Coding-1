b779a20f4cb06c3c4a9919fa02162acfe272c5ed
Nick30035 <nicholas@bbcdayschool.org.in>
2025-01-13 18:57:57 +0000

    asyncActivityJan10.js

--- Unit 4/asyncActivityJan10.js
+++ Unit 4/asyncActivityJan10.js
@@ -1,33 +1,24 @@
 // Create a list that includes 5 items that you can find in the frozen food isle.
 
 var groceryList= ['hot pockets, pizza rolls, frozen salmon,healthy choice,chicken breast']
-var RandomData = [' Nick', 202020,
-    202,323,
-'coding class',
-true, 
-false,
-groceryList
-]   
-
-
 var internationalGroceryList=['ground beef, seasoning,white rice, brown rice,chicken']
-var RandomData=['Food',202020,
-        202,323,
-       'international',
-    true,
-    false,
-internationalGroceryList
-]
-
-
 var produceList=['broccoli,strawberries,blueberries,carrots,cucumbers']
-var RandomData=['Produce',909292,
-    292,210,
-    'produce aisle',
-    veggies,
-    fruits,
-    produceList
-]
 
+groceryList=[internationalGroceryList,produceList];
+console.log(groceryList)
  
+
+2.[1,30,39,50,293,100];
+
+// print/ console.log the number at index 0
+1
+//print/ console.log the number at index 3
+50
+//print/ console.log the number at index 4
+293
+
+ function multiplier(x){
+    console.log(x*293);
+ }
+ multiplier(0)
     
