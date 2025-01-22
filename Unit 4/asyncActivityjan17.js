5a338f5aed8da5bde3bde68db230691f0d52fdf4
Nick30035 <nicholas@bbcdayschool.org.in>
2025-01-22 20:17:40 +0000

    whileLoops.js

---
+++ Unit 4/asyncActivityjan17.js
@@ -0,0 +1,9 @@
+// 1. campingSupplies= ['tent,sleeping bag,flash light,camping knif
+campingSupplies = ['tent,sleeping bag,flash light,camping knife]'
+
+
+// 2. add a new element to camping array
+reverseCampingList.push('water bottle')
+console.log(reverseCampingList)
+
+// 3. combine the array above with the array below
---
+++ Unit 4/whileLoops.js
@@ -0,0 +1,5 @@
+x = 2 // This is an example of a assignment operator - giving a variable value
+while(x=2){
+    console.log('This message will repeat forever')
+}
+
