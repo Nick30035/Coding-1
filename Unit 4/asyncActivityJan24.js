2401c237ab258eeb0ff41e2bf6fba60f40d7eb46
Nick30035 <nicholas@bbcdayschool.org.in>
2025-01-24 20:14:22 +0000

    asyncActivityJan24.js

---
+++ Unit 4/asyncActivityJan24.js
@@ -0,0 +1,8 @@
+function timer(){
+    var interval = 5
+    while (interval > -1){
+        console.log('countdown' + interval)
+        interval -=1
+    }
+}
+timer()
--- Unit 4/whileLoops.js
+++ Unit 4/whileLoops.js
@@ -3,3 +3,12 @@ while(x=2){
     console.log('This message will repeat forever')
 }
 
+function timer(){
+    var interval = 60
+    while (interval > 0){
+        console.log('countdown' + interval)
+        interval -=1
+    }
+    console.log("Times up!")
+}
+
