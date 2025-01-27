ebe353a6beca3719043b2434e23b8d6c8c261e3b
Nick30035 <nicholas@bbcdayschool.org.in>
2025-01-27 18:57:52 +0000

    asyncActivityJan24.js

--- Unit 4/asyncActivityJan24.js
+++ Unit 4/asyncActivityJan24.js
@@ -1,8 +1,14 @@
-function timer(){
-    var interval = 5
-    while (interval > -1){
-        console.log('countdown' + interval)
-        interval -=1
+var numberList = [1,23,56,3,56,3,20,200]
+
+reverseNumberList = numberList.reverse()
+console.log(reverseNumberList)
+
+function reverseNumbers(){
+    var ListNumber = 7
+    while ( ListNumber >= 0){
+        console.log(numberList[ListNumber])
+        ListNumber -=1
     }
 }
-timer()
+
+reverseNumbers()
